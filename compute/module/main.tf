b23364c364923ac1e91ce8e1bbfddb15d230c7e5
dackbusch <dbusch@qumulo.com>
2025-10-16 16:24:28 -0700

    Add support for z3-highmem-88

--- compute/module/main.tf
+++ compute/module/main.tf
@@ -20,7 +20,7 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-# **** Version 1.3 ****
+# **** Version 1.4 ****
 
 resource "null_resource" "project_lock" {
   triggers = {
